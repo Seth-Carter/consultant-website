508b179fcd97f010a9c6b27e258bbf0efd1839e0
Seth Carter <carter.seth.m@gmail.com>
2023-03-04 15:45:52 +0000

    feat: initial commit and work on nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Layout from '../components/layout';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component
+        /* eslint-disable-line react/jsx-props-no-spreading */ {...pageProps}
+      />
+    </Layout>
+  );
 }
